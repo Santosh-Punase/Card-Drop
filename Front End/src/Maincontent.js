5e075f05d41c5815f1d4d032b4a4c8435c3cd916
santosh-punase <31465976+Santosh-Punase@users.noreply.github.com>
2017-10-11 14:15:08 +0530

    cards added

--- Front End/src/Maincontent.js
+++ Front End/src/Maincontent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Container from 'muicss/lib/react/container';
-import Button from 'react-bootstrap/lib/Button';
 
+import Button from 'react-bootstrap/lib/Button';
+import Card from './Picked';
 
 {/*import './userpage.css';
 */}
@@ -11,6 +12,7 @@ const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
 class Maincontent extends React.Component {
   render() {
     return (
+        <div className="container">
       <Container className="mui--z1" >
          
          <div className="well" style={wellStyles}>
@@ -18,8 +20,11 @@ class Maincontent extends React.Component {
     <span class="glyphicon glyphicon-search"></span> Search nearby cards</Button>
     
   </div>
-        
+  
       </Container>
+      < Card />
+      < Card />
+      </div>
     );
   }
 }
