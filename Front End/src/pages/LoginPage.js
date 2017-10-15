29d5077a8f08438e54920876a50dd4052a3ea53e
santosh-punase <31465976+Santosh-Punase@users.noreply.github.com>
2017-10-15 16:24:37 +0530

    login auth

--- Front End/src/pages/LoginPage.js
+++ Front End/src/pages/LoginPage.js
@@ -2,7 +2,73 @@ import React from 'react';
 import './loginpage.css'
 
 class LoginPage extends React.Component {
-    
+    loginUser = function() {
+		var phone = document.getElementById('user').value;
+		var password = document.getElementById('pass').value;
+		
+		var UsrWarning = document.getElementById('warning-label-user')
+		var pwdWarning = document.getElementById('warning-label-pass')
+
+		var req = {
+			"phone":phone,
+			"password":password
+		};
+		//console.log(JSON.stringify(req));
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://carddrop.herokuapp.com/api/users/login", true);
+		xhr.setRequestHeader("Content-Type","application/json");
+		xhr.setRequestHeader("Access-Control-Allow-Origin","*");		
+		xhr.onreadystatechange = function () {
+			if(xhr.readyState === 4) {
+				if(xhr.status === 200) {
+					var res = xhr.responseText;
+					var Jres = JSON.parse(res);
+					if(Jres["status"]=="NOT_REGISTERED")
+						UsrWarning.innerHTML = "No User Found";
+					else if(Jres["status"]=="INVALID_PASSWORD")
+						UsrWarning.innerHTML = "Invalid Password";
+						else						
+						UsrWarning.innerHTML = "";
+
+					console.log(Jres["status"]);
+				}
+			}
+		}
+		xhr.send(JSON.stringify(req));
+	}
+
+	regUser = function() {
+		var phone = document.getElementById('user').value;
+		var password = document.getElementById('pass').value;
+		
+		var UsrWarning = document.getElementById('warning-label-user')
+		
+		var req = {
+			"phone":phone,
+			"password":password
+		};
+		//console.log(JSON.stringify(req));
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://carddrop.herokuapp.com/api/users/register", true);
+		xhr.setRequestHeader("Content-Type","application/json");
+		xhr.setRequestHeader("Access-Control-Allow-Origin","*");		
+		xhr.onreadystatechange = function () {
+			if(xhr.readyState === 4) {
+				if(xhr.status === 200) {
+					var res = xhr.responseText;
+					var Jres = JSON.parse(res);
+					if(Jres["status"]=="ALREADY_REGISTERED")
+						UsrWarning.innerHTML = "User Already Exixts";
+					else						
+						UsrWarning.innerHTML = "";
+
+					console.log(Jres["status"]);
+				}
+			}
+		}
+		xhr.send(JSON.stringify(req));
+	}
+
       render () {
         return (
 			<div class="login-wrap">
@@ -12,19 +78,21 @@ class LoginPage extends React.Component {
 					<div class="login-form">
 						<div class="sign-in-htm">
 							<div class="group">
-								<label for="user" class="label">Username</label>
+								<label for="user" class="label">Mobile Number</label>
 								<input id="user" type="text" class="input" />
+								<div id="warning-label-user"></div>
 							</div>
 							<div class="group">
 								<label for="pass" class="label">Password</label>
 								<input id="pass" type="password" class="input" data-type="password" />
+								<div id="warning-label-pass"></div>
 							</div>
 							<div class="group">
 								<input id="check" type="checkbox" class="check" checked />
 								<label for="check"><span class="icon"></span> Keep me Signed in</label>
 							</div>
 							<div class="group">
-								<input type="submit" class="button" value="Sign In" />
+								<input type="submit" class="button" value="Sign In" onClick={this.loginUser.bind(this)}/>
 							</div>
 							<div class="hr"></div>
 							<div class="foot-lnk">
@@ -33,8 +101,9 @@ class LoginPage extends React.Component {
 						</div>
 						<div class="sign-up-htm">
 							<div class="group">
-								<label for="user" class="label">Username</label>
+								<label for="user" class="label">Mobile Number</label>
 								<input id="user" type="text" class="input" />
+								<div id="warning-label-user"></div>
 							</div>
 							<div class="group">
 								<label for="pass" class="label">Password</label>
@@ -49,11 +118,11 @@ class LoginPage extends React.Component {
 								<input id="pass" type="text" class="input" />
 							</div>
 							<div class="group">
-								<input type="submit" class="button" value="Sign Up" />
+								<input type="submit" class="button" value="Sign Up"  onClick={this.regUser.bind(this)} />
 							</div>
 							<div class="hr"></div>
 							<div class="foot-lnk">
-								<label for="tab-1" /><a>Already Member?</a>
+								<label for="tab-1" ><a>Already Member?</a></label>
 							</div>
 						</div>
 					</div>
